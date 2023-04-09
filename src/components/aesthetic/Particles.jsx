2a406d1df273729c7d29e192662cde25242b4389
johnschatner <leontin2001@gmail.com>
2023-04-09 17:45:11 +0200

    Wave 9

--- src/components/aesthetic/Particles.jsx
+++ src/components/aesthetic/Particles.jsx
@@ -1,33 +1,61 @@
-import React, { useRef, useMemo, useEffect } from "react";
+import React, { useRef, useMemo, useState, useEffect } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import * as THREE from "three";
+import Stats from "stats.js";
 
 const ParticleSystem = () => {
   const particles = useRef();
-  const count = 10000; // Number of particles
-  const gridSize = Math.sqrt(count);
+  const count = 22100; // Number of particles
+  const particleSpacing = 1;
+  const gridSize = Math.sqrt(count * 1.6);
+
+  const [particleColor, setParticleColor] = useState({
+    r: 0.7,
+    g: 1.0,
+    b: 1.0,
+  });
+
+  const updateParticleColor = () => {
+    if (document.body.classList.contains("dark")) {
+      setParticleColor({ r: 0.7, g: 1.0, b: 1.0 }); // Set color to black
+    } else {
+      setParticleColor({ r: 0, g: 0, b: 0 }); // Set color to original color
+    }
+  };
+
+  useEffect(() => {
+    updateParticleColor(); // Update the color initially
+  }, []);
 
   const { viewport } = useThree();
   const pixelRatio = window.devicePixelRatio;
 
-  const positions = useMemo(() => {
-    const pos = new Float32Array(count * 3);
-    const gridSize = Math.sqrt(count);
+  const positions = useRef(new Float32Array(count * 3));
 
-    // Calculate the dimensions of the canvas
-    const width = viewport.width / pixelRatio;
-    const height = viewport.height / pixelRatio;
+  useEffect(() => {
+    const updatePositions = () => {
+      const width = (viewport.width / pixelRatio) * particleSpacing;
+      const height = (viewport.height / pixelRatio) * particleSpacing;
 
-    for (let i = 0, idx = 0; i < gridSize; i++) {
-      for (let j = 0; j < gridSize; j++, idx += 3) {
-        // Update positions to fill the canvas
-        pos[idx] = (i / (gridSize - 2)) * width - width / 2;
-        pos[idx + 1] = (j / (gridSize - 1)) * height - height / 2;
-        pos[idx + 2] = 0;
+      for (let i = 0, idx = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++, idx += 3) {
+          positions.current[idx] = (i / (gridSize - 2)) * width - width / 2;
+          positions.current[idx + 1] =
+            (j / (gridSize - 1)) * height - height / 2;
+          positions.current[idx + 2] = 0;
+        }
       }
-    }
-    return pos;
-  }, [count, viewport, pixelRatio]);
+    };
+
+    updatePositions();
+
+    const handleResize = () => {
+      updatePositions();
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [viewport, pixelRatio, gridSize, particleSpacing]);
 
   const vertexShader = `
   uniform float time;
@@ -44,27 +72,13 @@ const ParticleSystem = () => {
 
   const fragmentShader = `
   uniform float time;
+  uniform vec3 particleColor;
   varying float vOpacity;
   
   void main() {
-    gl_FragColor = vec4(0.7, 1.0, 1.0, vOpacity);
+    gl_FragColor = vec4(particleColor, vOpacity);
   }
 `;
-
-  const material = new THREE.ShaderMaterial({
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    uniforms: {
-      time: { value: 0 },
-      aspect: { value: 1 },
-    },
-    transparent: true,
-  });
-
-  const geometry = new THREE.BufferGeometry().setAttribute(
-    "position",
-    new THREE.BufferAttribute(positions, 3)
-  );
 
   const waveHeights = new Float32Array(count);
   const waveOffsets = new Float32Array(count * 2);
@@ -75,27 +89,70 @@ const ParticleSystem = () => {
     // create waves with random amplitude
     waveHeights[i] = 0.05 + Math.random() * 0.5;
   }
-
-  geometry.setAttribute(
-    "waveOffset",
-    new THREE.BufferAttribute(waveOffsets, 2)
-  );
-  geometry.setAttribute(
-    "waveHeight",
-    new THREE.BufferAttribute(waveHeights, 1)
-  );
-
-  const points = new THREE.Points(geometry, material);
   const opacityArray = new Float32Array(count);
   opacityArray.fill(0);
-  geometry.setAttribute("opacity", new THREE.BufferAttribute(opacityArray, 1));
+
+  const materialRef = useRef(null);
+  const geometryRef = useRef(null);
+
+  const material = useMemo(() => {
+    const material = new THREE.ShaderMaterial({
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader,
+      uniforms: {
+        time: { value: 0 },
+        aspect: { value: 1 },
+        particleColor: {
+          value: new THREE.Vector3(
+            particleColor.r,
+            particleColor.g,
+            particleColor.b
+          ),
+        },
+      },
+      transparent: true,
+    });
+
+    materialRef.current = material;
+    return material;
+  }, []);
+
+  const geometry = useMemo(() => {
+    const geometry = new THREE.BufferGeometry().setAttribute(
+      "position",
+      new THREE.BufferAttribute(positions.current, 3)
+    );
+
+    geometry.setAttribute(
+      "waveOffset",
+      new THREE.BufferAttribute(waveOffsets, 2)
+    );
+    geometry.setAttribute(
+      "waveHeight",
+      new THREE.BufferAttribute(waveHeights, 1)
+    );
+    geometry.setAttribute(
+      "opacity",
+      new THREE.BufferAttribute(opacityArray, 1)
+    );
+
+    geometryRef.current = geometry;
+    return geometry;
+  }, []);
+
+  const points = new THREE.Points(geometry, material);
 
   useFrame((state) => {
-    const time = state.clock.getElapsedTime() * 0.5;
+    const time = state.clock.getElapsedTime() * 0.4;
     const { width, height } = state.viewport;
 
     for (let i = 0, idx = 0; i < gridSize; i++) {
       for (let j = 0; j < gridSize; j++, idx += 3) {
+        particles.current.geometry.attributes.position.array[idx] =
+          positions.current[idx];
+        particles.current.geometry.attributes.position.array[idx + 2] =
+          positions.current[idx + 1];
+
         const x = particles.current.geometry.attributes.position.array[idx];
         const y = particles.current.geometry.attributes.position.array[idx + 1];
 
@@ -105,16 +162,16 @@ const ParticleSystem = () => {
           Math.sin(time * 1.5 + x * 0.25) *
           Math.cos(time * 1.5 + y * 0.25);
         const wave3 =
-          0.5 *
+          2 *
           Math.sin(time * 0.75 + x * 0.75) *
           Math.cos(time * 0.75 + y * 0.75);
 
         const waveHeight = (wave1 + wave2 + wave3) * 0.2;
 
         particles.current.geometry.attributes.position.array[idx + 2] =
-          waveHeight * 0.05;
+          waveHeight * 0.02;
 
-        const opacity = waveHeight < 0 ? 0 : waveHeight * 1;
+        const opacity = waveHeight < 0 ? 0.05 : waveHeight * 1;
 
         particles.current.geometry.attributes.opacity.array[idx / 3] = opacity;
       }
@@ -136,33 +193,29 @@ const ParticleSystem = () => {
 };
 
 const Particles = () => {
-  const canvasRef = useRef(null);
-
   useEffect(() => {
-    const handleResize = () => {
-      const canvas = canvasRef.current;
+    const stats = new Stats();
+    stats.showPanel(0); // Show FPS panel (0: fps, 1: ms, 2: mb, 3+: custom)
+    document.body.appendChild(stats.dom);
 
-      if (!canvas) {
-        return;
-      }
-
-      canvas.style.width = "100%";
-      canvas.style.height = "100%";
-      canvas.width = canvas.offsetWidth;
-      canvas.height = canvas.offsetHeight;
+    const animate = () => {
+      stats.begin();
+      stats.end();
+      requestAnimationFrame(animate);
     };
 
-    handleResize();
+    requestAnimationFrame(animate);
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    return () => {
+      document.body.removeChild(stats.dom);
+    };
   }, []);
 
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <Canvas
         style={{ position: "absolute", top: 0, left: 0 }}
-        camera={{ position: [0, 0, 20], fov: 75 }}
+        camera={{ position: [0, 0, 15], fov: 75 }}
         onCreated={({ gl }) => {
           gl.setPixelRatio(window.devicePixelRatio);
           gl.setSize(window.innerWidth, window.innerHeight);
