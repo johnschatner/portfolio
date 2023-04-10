13c3aaaf49e621676923164d5cbc8f3b91520ce5
johnschatner <leontin2001@gmail.com>
2023-04-10 15:42:38 +0200

    Height issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import Noise from "./components/aesthetic/Noise";
 import PortfolioRouter from "./Pages/PortfolioRouter";
 
 function App() {
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  window.addEventListener("resize", appHeight);
+  appHeight();
+
   return (
     <div className="App">
       <PortfolioRouter></PortfolioRouter>
