f8b7ded5949293ca36355c07e5bd73fcb095d093
johnschatner <leontin2001@gmail.com>
2023-04-24 14:24:20 +0200

    Add intro animation to hide threejs initi artifacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 // Components
 import PortfolioRouter from "./Pages/PortfolioRouter";
 
 function App() {
+  const [introActive, setIntroActive] = useState(true);
+
+  useEffect(() => {
+    console.log(introActive);
+    setTimeout(() => {
+      setIntroActive(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
+      <div className={`intro ${introActive ? "active" : "inactive"}`}>
+        <div className="intro-title">
+          <span className="intro-name">Isac Rasmusson</span>
+          <span className="intro-subtitle">Portfolio</span>
+        </div>
+      </div>
       <PortfolioRouter></PortfolioRouter>
     </div>
   );
